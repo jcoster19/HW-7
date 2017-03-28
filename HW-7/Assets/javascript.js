ac6341e027634b4a23a07ceaa6ee8da277347e65
jcoster19 <jcoster19@gmail.com>
2017-03-28 16:36:59 -0400

    Add files via upload
    First push

---
+++ HW-7/Assets/javascript.js
@@ -0,0 +1,62 @@
+$(document).ready(function(){
+
+	var trainData = new Firebase("https://train-schedule-78076.firebaseio.com");
+
+	$("#addTrainBtn").on("click", function(){
+
+			var trainName = $("#trainNameInput").val().trim();
+			var destination = $("#destinationInput").val().trim();
+			var trainTimeInput = moment($("#trainTimeInput").val().trim(), "HH:mm").subtract(10, "years").format("X");
+			var frequencyInput = $("#frequencyInput").val().trim();
+
+			console.log(trainName);
+			console.log(destination);
+			console.log(trainTimeInput);
+			console.log(frequencyInput);
+
+			var newTrain = {
+
+				name: trainName,
+				destination: destination,
+				trainTime: trainTimeInput,
+				frequency: frequencyInput,
+			}
+
+			trainData.push(newTrain);
+
+			$("#trainNameInput").val("");
+			$("#destinationInput").val("");
+			$("#trainTimeInput").val("");
+			$("#frequencyInput").val("");
+
+			return false;
+
+	});
+
+
+	trainData.on("child_added", function(childSnapshot, prevChildKey){
+
+		console.log(childSnapshot.val());
+
+		var firebaseName = childSnapshot.val().name;
+		var firebaseDestination = childSnapshot.val().destination;
+		var firebaseTrainTimeInput = childSnapshot.val().trainTime;
+		var firebaseFrequency = childSnapshot.val().trainTime;
+
+		var diffTime = moment().diff(moment.unix(firebaseTrainTimeInput), "minutes");
+		var timeRemainder = moment().diff(moment.unix(firebaseTrainTimeInput), "minutes") % firebaseFrequency;
+		var minutes = firebaseFrequency - timeRemainder;
+
+		var nextTrainArrival = moment().add(minutes, "m").format("hh:mm A");
+
+		console.log(minutes);
+		console.log(nextTrainArrival);
+		console.log(moment().format("hh:mm A"));
+		console.log(nextTrainArrival);
+		console.log(moment().format("X"));
+
+		$("#trainTable > tbody").append("<tr><td>" + firebaseName + "</td><td>" + firebaseDestination + "</td><td>" + firebaseFrequency + " mins" + "</td><td>" + nextTrainArrival + "</td><td>" + minutes + "</td></tr>");
+	});
+
+
+});
